749baf9b6d92358cfbd71a92c865c5cabe8196ab
Ali Hashemi <alihashemmi.ca@gmail.com>
2022-02-01 21:52:27 -0500

    adding tweet card

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+  $("#tweet-text")
+    .on("input", function() {
+
+      this.style.height = "";
+      this.style.height = this.scrollHeight + 4 + "px";
+
+      // Counter value
+      const counter = $(this).next('div').children('output')[0];
+      counter.value = 140 - this.value.length;
+
+      // color change
+      if (counter.value < 0) {
+        $(counter).css("color", "red");
+      }
+    });
+});
